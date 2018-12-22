39c09cc5f050e519be280839ea87b8b5e14108d5
otoshuki <guining.pertin.iitg@gmail.com>
2018-12-23 03:10:36 +0530

    Baseline construction using Porcupine

--- hiyoka_main.py
+++ hiyoka_main.py
@@ -3,12 +3,28 @@ import wake_word as ww
 import nltk
 import random
 import numpy as np
-from watson_developer_cloud import TextToSpeechV1
-from googletrans import Translator as Ts
-import speech_recognition as sr
-import json
-'''
-text_to_speech = TextToSpeechV1(
-    username=username='d816def2-ebc4-4fec-8845-93362a41fb18',
-    password='MdhCKRSjNKkx')
-'''
+
+#Detect wake wake_word
+#Detect welcome home trigger
+#Detect good morning
+#Detect good night
+#Speak
+
+#Wait for command
+#Speak
+
+#Perform command
+
+#Play Music
+
+#Control lights
+
+#Control extension cord
+
+#Say a joke
+
+#Say a story
+
+#Weather forecast
+
+#Scheduling
